58231acc8583c2c6243c83712ce56c665ddb5b24
Pratham08 <prathampali0909@gmail.com>
2023-08-23 03:24:24 +0530

    first commit

---
+++ models/Event.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const EventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  date: {
+    type: String,
+  },
+  time: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  organiser: {
+    type: String,
+  },
+  event_organization :{
+    type: String,
+  },
+  registrations: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Action = mongoose.model("action", EventSchema);
+module.exports = Action;
---
+++ routes/event.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const Action = require("../models/Event");
+router.post("/register", async (req, res) => {
+  try {
+    const {name,description,location,date,time,organizer,registrations,event_organization} = req.body;
+    const event = new Action({name: name,description: description,location: location,date: date,time: time,organiser: organizer,registrations: registrations,event_organization: event_organization});
+    console.log(event);
+    const savedEvent = await event.save();
+    res.json(savedEvent);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/get-event", async (req, res) => {
+  const eve = await Action.find({});
+  return res.send(eve);
+});
+
+module.exports = router;
